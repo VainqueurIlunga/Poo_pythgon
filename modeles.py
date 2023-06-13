e762199ac121017ee5fd6d96e815c557389184d3
Fim Vainqueur Ilunga <ilungafranck236@gmail.com>
2023-06-13 15:08:54 +0200

    ajout du model  injscrit

--- modeles.py
+++ modeles.py
@@ -2,17 +2,57 @@ from abc import ABC, abstractmethod
 import uuid
 
 class User(ABC):
+    def __init__(self, user_name, user_last_name, mail):
+        self.name = user_name
+        self.last_name= user_last_name
+        self.mail = mail
+
+class GenerateurMatric(ABC):
+    """
     def __init__(self):
+        self.Matric = self.Gen_matric()
+
+    @staticmethod
+    def Gen_matric():
+        return str(uuid.uuid4())
+
+    """
+    @abstractmethod
+    def Gen_matric(self):
         pass
 
-class APC(User):
-    pass
-        
-class Etudiant(User):
-    pass
 
-class SecCompt(User):
-    pass
+
+
+class Apc(GenerateurMatric, User):
+    def __init__(self,user_name, user_last_name, mail):
+        super().__init__(user_name, user_last_name, mail)
+        self.Matric = self.Gen_matric()
+
+    def Gen_matric():
+        return str(uuid.uuid4())
+
+
+    
+        
+class Etudiant(GenerateurMatric, User):
+    def __init__(self,user_name, user_last_name, mail):
+        super().__init__(user_name, user_last_name, mail)
+        self.Matric = self.Gen_matric()
+
+    def Gen_matric():
+        return str(uuid.uuid4())
+
+
+class SecCompte(GenerateurMatric,User):
+    def __init__(self,user_name, user_last_name, mail):
+        User.__init__(self,user_name, user_last_name, mail)
+        self.Matric = self.Gen_matric()
+
+    def Gen_matric():
+        return str(uuid.uuid4())
+
+        
 
 class GestionInscription:
     pass
@@ -31,3 +71,17 @@ class Faculte:
 
 class Departement:
     pass
+
+
+"""
+use= User("ilunga","Vainqueur","ilunga@gmail.com")
+print(use)
+"""
+
+"""
+lido = SecCompte("estha", "ether", "estha@gmail.com")
+print(lido.Gen_matric())
+"""
+
+lido = SecCompte("ggg","huffd","hfhhfhf")
+print(lido.Gen_matric())
